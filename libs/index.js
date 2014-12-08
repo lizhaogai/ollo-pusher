c2f98738dac7052b9ef42fc5538555a754fe2160
yifeng <493788939lizhaogai@163.com>
2014-12-08 15:02:21 +0800

    c

--- libs/index.js
+++ libs/index.js
@@ -1,7 +1,6 @@
 "use strict";
 
 exports.init = function (napp, app) {
-    napp.module(__dirname);
     var opts = app.get('pusher');
     var appKey = opts.appKey;
     var appSecret = opts.appSecret;
